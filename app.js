31574ad62385203991c944cbe70ef09cf1f020d1
Tyler <tbuchhei@alumni.nd.edu>
2018-01-03 10:07:21 -0600

    Implement index with connected db

--- app.js
+++ app.js
@@ -3,3 +3,35 @@ const express     = require("express"),
       mongoose    = require("mongoose"),
       app         = express();
       
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const blogSchema = new mongoose.Schema({
+      title: String,
+      image: String,
+      body: String,
+      created: {type: Date, default: Date.now}
+});
+const Blog = mongoose.model("Blog", blogSchema);
+
+
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {blogs});
+    }
+  });
+});
+
+
+
+app.listen(3000, () => console.log("RESTful blog running on port 3000"));
+
